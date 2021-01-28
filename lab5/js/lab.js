214f7440b30712c35d6665b5de9f00a1246d2b3b
elenaibbo <elena.ibbo@gmail.com>
2021-01-27 22:30:44 -0800

    lab 5
    made lab5 html with link to lab 5 js file

---
+++ lab5/js/lab.js
@@ -0,0 +1,21 @@
+/**
+ * Author:    Elena Ibbetson
+ * Created:   1.27.2021
+ *
+ *
+ **/
+
+ // Define Variables
+ var make = "Mazda";
+ var model = "5";
+ var color = "White";
+ var year = 2013;
+
+ //calculate
+ var age = 2021 - year;
+
+ //output
+ document.writeln("Make: " + make + "<br>");
+ document.writeln("Model: " + model + "<br>");
+ document.writeln("Color: " + color + "<br>");
+ document.writeln("Year: " + year + "<br>");
